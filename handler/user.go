69d982547209fb40ef614a6875bd4215c8be28fb
themordhon <udahmakandon@gmail.com>
2021-07-28 23:37:28 +0700

    Init Project Register User End Point

---
+++ handler/user.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"farmingcrowdfunding/helper"
+	"farmingcrowdfunding/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap input dari user
+	// map input dari user ke struct ke register user input
+	// struct diatas kita passing sebagai parameter service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// toker, err := h.jwtService.GenerateToker()
+
+	formater := user.FormatUser(newUser, "tokentokentoken")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "Success", formater)
+
+	c.JSON(http.StatusOK, response)
+}

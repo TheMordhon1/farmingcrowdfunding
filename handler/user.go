30e7f6f8b83fb90c5e815124475041426dd53af5
themordhon <udahmakandon@gmail.com>
2021-07-29 16:41:34 +0700

    Login Endpoint

--- handler/user.go
+++ handler/user.go
@@ -26,7 +26,6 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
 		errors := helper.FormatValidationError(err)
-
 		errorMessage := gin.H{"errors": errors}
 
 		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
@@ -42,11 +41,47 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		return
 	}
 
-	// toker, err := h.jwtService.GenerateToker()
+	// token, err := h.jwtService.GenerateToken()
 
 	formater := user.FormatUser(newUser, "tokentokentoken")
 
 	response := helper.APIResponse("Account has been registered", http.StatusOK, "Success", formater)
+
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	// user memasukan input (email & psw)
+	// Input ditangkap hadler
+	// mapping dari input user ke input struct
+	// Input struct passing ke service
+	// di service mencari dengan bantuan repository user dengan email x
+	// mencocokan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formater := user.FormatUser(loggedinUser, "tokentokentoken")
+
+	response := helper.APIResponse("Successfully loggedin", http.StatusOK, "Success", formater)
 
 	c.JSON(http.StatusOK, response)
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"farmingcrowdfunding/handler"
 	"farmingcrowdfunding/user"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -21,18 +22,27 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 
+	input := user.LoginInput{
+		Email:    "galih@gmail.com",
+		Password: "password",
+	}
+	user, err := userService.Login(input)
+	if err != nil {
+		fmt.Println("Terjadi Kesalahan")
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(user.Email)
+	fmt.Println(user.Name)
+
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
-	router.Run()
+	api.POST("/sessions", userHandler.Login)
 
-	// input dari user
-	// handler : mapping input dari user menjadi sebuah struct input
-	// service : melakukan mapping dari struct input ke struct user
-	// repository
-	// db
+	router.Run()
 
 }

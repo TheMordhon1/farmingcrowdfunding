38ae7aa6ad9ed88feb92603e5544b6dc8b4e52ee
themordhon <udahmakandon@gmail.com>
2021-07-30 23:07:52 +0700

    jwt auth, validate token

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"farmingcrowdfunding/auth"
 	"farmingcrowdfunding/handler"
 	"farmingcrowdfunding/user"
-	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -23,21 +22,6 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
-
-	fmt.Println(authService.GenerateToken(1001))
-
-	input := user.LoginInput{
-		Email:    "dian@gmail.com",
-		Password: "password",
-	}
-	user, err := userService.Login(input)
-	if err != nil {
-		fmt.Println("Terjadi Kesalahan")
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println(user.Email)
-	fmt.Println(user.Name)
 
 	userHandler := handler.NewUserHandler(userService, authService)
 

54e4a068cf956f1fc0b18e58cf25fa7156ec6f95
themordhon <udahmakandon@gmail.com>
2021-08-02 17:57:41 +0700

    Get campaigns endpoint (Work In Progress)

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"farmingcrowdfunding/handler"
 	"farmingcrowdfunding/helper"
 	"farmingcrowdfunding/user"
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -32,10 +31,8 @@ func main() {
 	CampaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
 
-	campaigns, _ := CampaignService.FindCampaigns(26)
-	fmt.Println(len(campaigns))
-
 	userHandler := handler.NewUserHandler(userService, authService)
+	campaignHandler := handler.NewCampaignHandler(CampaignService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
@@ -45,6 +42,8 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+
+	api.GET("/campaigns", campaignHandler.GetCampaigns)
 
 	router.Run()
 
